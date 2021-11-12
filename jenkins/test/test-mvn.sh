6624798e38ab199085c7707afbc78c635cfff449
lgustavosjc <85200721+lgustavosjc@users.noreply.github.com>
2021-11-12 01:47:25 -0300

    Update test-mvn.sh

--- jenkins/test/test-mvn.sh
+++ jenkins/test/test-mvn.sh
@@ -5,3 +5,5 @@ echo "***************************"
 WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/vorx-backend-app-pipeline
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
 
+
+
